e585cc838b38daa641262c67e0c9f3a00d691f68
Robinson Lourenço <robinson.lourencoti@gmail.com>
2025-05-06 20:33:02 -0300

    Refactor domain classes and add Docker support
    Changed `Categoria` and `Produto` classes to public and added XML documentation. Introduced relationships between the two classes. Updated the solution file to include a reference to `docker-compose.yml`. Added a new `docker-compose.yml` file to configure a SQL Server service with persistent storage and network settings.

--- ProdutosApp.Domain/Entities/Categoria.cs
+++ ProdutosApp.Domain/Entities/Categoria.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace ProdutosApp.Domain.Entities
 {
-    internal class Categoria
+    /// <summary>
+    /// Represents a product category model.
+    /// </summary>
+    public class Categoria
     {
+        #region Properties
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Nome { get; set; } = string.Empty;
+        public string? Descricao { get; set; }
+
+        #endregion
+
+        #region Relations
+        public ICollection<Produto>? Produtos { get; set; }
+        #endregion
     }
 }
--- ProdutosApp.Domain/Entities/Produto.cs
+++ ProdutosApp.Domain/Entities/Produto.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace ProdutosApp.Domain.Entities
 {
-    internal class Produto
+    /// <summary>
+    /// Represents a product model.
+    /// </summary>
+    public class Produto
     {
+        #region Properties
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Nome { get; set; } = string.Empty;
+        public decimal? Preco { get; set; }
+        public int? Quantidade { get; set; }
+        public DateTime? DataHoraCriacao { get; set; } // DateOnly + TimeSpan
+        public bool? Ativo { get; set; }
+        public Guid? CategoriaId  { get; set; }
+        #endregion
+
+        #region Relations
+        public Categoria? Categoria { get; set; }
+        #endregion
     }
 }
