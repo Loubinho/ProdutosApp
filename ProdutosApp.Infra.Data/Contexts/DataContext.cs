145243ca17c7dd09f3312665741453fed78cd54e
Robinson Lourenço <robinson.lourencoti@gmail.com>
2025-05-08 21:51:07 -0300

    Refactor data context and add EF Core extension
    Removed folder references in the project file. Added a new `DataContext` class for Entity Framework Core, including entity configurations for `Produto` and `Categoria`. Introduced `EntityFrameworkExtension` for registering EF Core services with SQL Server configuration.

---
+++ ProdutosApp.Infra.Data/Contexts/DataContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProdutosApp.Infra.Data.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdutosApp.Infra.Data.Contexts
+{
+    /// <summary>
+    /// Represents the database context for the application with Entity Framework Core.
+    /// </summary>
+    public class DataContext : DbContext
+    {
+        /// <summary>
+        /// Constructor for the DataContext class.
+        /// </summary>
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        /// <summary>
+        /// DbSet for the Produto entity.
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProdutoMap());
+            modelBuilder.ApplyConfiguration(new CategoriaMap());
+
+            // Apply the mappings
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ ProdutosApp.Infra.Data/Extensions/EntityFrameworkExtension.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdutosApp.Infra.Data.Extensions
+{
+    /// <summary>
+    /// Extension class for adding Entity Framework Core services to the IServiceCollection.
+    /// </summary>
+    public static class EntityFrameworkExtension
+    {
+        /// <summary>
+        /// Extension method to add Entity Framework Core services to the IServiceCollection.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddEntityFramewor( this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add Entity Framework Core with SQL Server
+            services.AddDbContext<Contexts.DataContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("ProdutosAppBD")));
+
+            // Add other services related to Entity Framework Core here if needed
+            return services;
+        }
+    }
+}
