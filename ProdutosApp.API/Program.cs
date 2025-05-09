25e19fd783752029ca1ad558e3c4617fff57a71b
Robinson Lourenço <robinson.lourencoti@gmail.com>
2025-05-09 19:27:19 -0300

    Integrate Entity Framework and update project structure
    - Added `Microsoft.EntityFrameworkCore.Design` package and project reference to `ProdutosApp.Infra.Data`.
    - Updated `Program.cs` to include Entity Framework services and modified Swagger configuration.
    - Changed connection string in `appsettings.Development.json` to use `ProdutoDB`.
    - Removed folder reference for `Repositories` in `ProdutosApp.Infra.Data.csproj`.
    - Created initial migration `20250509214305_Initial.cs` with `Categorias` and `Produtos` tables.
    - Generated supporting files for the migration: `20250509214305_Initial.Designer.cs` and `DataContextModelSnapshot.cs`.
    - Added new repository classes: `BaseRepository`, `CategoriaRepository`, and `ProdutoRepository`.

--- ProdutosApp.API/Program.cs
+++ ProdutosApp.API/Program.cs
@@ -1,54 +1,49 @@
+using ProdutosApp.Infra.Data.Extensions;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-
+//Registrando os serviços de injeção de dependência
 builder.Services.AddEntityFramework(builder.Configuration);
 
 //Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(
-//c =>
-//{
-//    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
-//    {
-//        Title = "ProdutosApp API",
-//        Version = "v1"
-//    });
-//}
-);
+    //c =>
+    //{
+    //    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    //    {
+    //        Title = "ProdutosApp API",
+    //        Version = "v1"
+    //    });
+    //}
+    );
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-//swagger
+
+//Swagger
 app.UseSwagger();
 app.UseSwaggerUI(
-//    c =>
-//{
-//    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProdutosApp API V1");
-//    c.RoutePrefix = string.Empty; // Set the Swagger UI at the app's root
-//}
-);
+    //    c =>
+    //{
+    //    c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProdutosApp API V1");
+    //    c.RoutePrefix = string.Empty; // Set the Swagger UI at the app's root
+    //}
+    );
 
 //Scalar
-app.MapScalarApiReference(options =>
-{
+app.MapScalarApiReference(options => {
     options.WithTheme(ScalarTheme.BluePlanet);
 });
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
---
+++ ProdutosApp.Infra.Data/Repositories/CategoriaRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdutosApp.Infra.Data.Repositories
+{
+    public class CategoriaRepository
+    {
+    }
+}
